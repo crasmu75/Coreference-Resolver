5125f2d1d4bad9739da5346e6557ad5318da0830
crasmu75 <camillerasmussen@live.com>
2016-11-05 16:30:19 -0600

    Project files, who knows. And method for gender
    I also tried to get the regex to match the tags bc you were right the
    XML parsers don’t work.

--- coreference.py
+++ coreference.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
 import string
 import sys
 import os
+import re
 
 # future hopes and dreams:
 # Looking for proper nouns (nouns capitalized not at beginning of sentence)
@@ -50,6 +52,29 @@ pronouns = {"reflexive": {"singular": ["myself",
                                       "them"]}
             }
 
+masculine_words = ["male",
+                   "boy",
+                   "man",
+                   "guy",
+                   "brother",
+                   "father",
+                   "uncle",
+                   "paternal",
+                   "macho",
+                   "masculine"]
+feminine_words = ["female",
+                  "girl",
+                  "woman",
+                  "lady",
+                  "sister",
+                  "mother",
+                  "aunt",
+                  "maternal",
+                  "feminine"]
+
+# dictionary for each tagged anaphor - maps to it's attributes
+anaphora = {}
+
 
 def remove_punctuation(s):
     exclude = set(string.punctuation)
@@ -66,6 +91,19 @@ def find_acronyms(phrase):
 
     return [f(phrase) for f in rules]
 
+
+# intended to take the entire phrase wrapped in the coref tags and an expected gender
+def gender_match(phrase, gender):
+    words = phrase.split()
+    for word in words:
+        word = word.replace("'s", "").replace("ly", "").replace("ish", "")
+        if word in masculine_words:
+            return gender == "male"
+        if word in feminine_words:
+            return gender == "female"
+    return gender == "genderless"
+
+
 # looking for over 50% similarity
 def overlap_similarity(phrase1, phrase2):
     phrase1 = set(remove_punctuation(phrase1).lower().split())
@@ -80,6 +118,14 @@ def find_coreferences(input_file, output_dir):
     outputFile = os.path.join(output_dir, filename + '.response')
 
     # Do stuff here
+
+    # weird quotes are causing the regular expression issues. ” vs "
+    # is currently not matching the beginning tags such as <COREF ID=”1”>
+    # also, XML parsers do not work on these files. There are foreign characters or something isn't formatted correctly
+    # in her files so regex will have to do.
+    # taking out the encoding declaration at the top of the file breaks this
+    targets = re.split("(</?COREF( ID=(”|\")[0-9](”|\"))?>)", contents)
+    # once this works, do some work to get our anaphora into the dictionary declared above???
     newContents = contents
 
     # Writes `newContents` to `outputFile`
@@ -96,13 +142,20 @@ def main():
     # for file in files:
     #     find_coreferences(file, outputDir)
 
-
+    # reading in example file from project description
+    # find_coreferences("example_input.txt", "Coreference-Resolver")
 
     print(find_acronyms('John F. Kennedy'))
     print(find_acronyms('National Aeoronautics and Space Administration'))
     print()
     print(overlap_similarity('John F. Kennedy', 'John Kennedy'))
     print(overlap_similarity('Ford Motor Co.', 'Ford'))
+
+    print(gender_match("The adult male's body", "male"))
+    print(gender_match("womanly presence", "female"))
+    print(gender_match("broken tree branch", "genderless"))
+    print(gender_match("The motherly person", "female"))
+    print(gender_match("The boyish toy", "female"))
 
 
 if __name__ == '__main__':
